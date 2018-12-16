db3243a1e7300d919678b0a2967ab55ef3988241
Emmanuel <mezamartinezzz@ciencias.unam.mx>
2018-12-16 17:20:38 -0600

    import de kevin

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -36,4 +36,49 @@ insert into ORIGEN(origen)VALUES('NO');
 insert into DESTINO(destino)VALUES('SI');
 insert into DESTINO(destino)VALUES('NO');
 
+INSERT INTO DIRECCION(calle,ciudad,colonia,cp,estado,exterior,interior,municipio)VALUES('Apartado Pretium Calle','Patarrá','SJ','12619','Yucatán',357,194,'San José');
+INSERT INTO DIRECCION(calle,ciudad,colonia,cp,estado,exterior,interior,municipio)VALUES('Apartado Vulputate ','San Diego','Cartago','30415','Hidalgo',960,454,'SJ');
+INSERT INTO DIRECCION(calle,ciudad,colonia,cp,estado,exterior,interior,municipio)VALUES('Apartado Lectus Calle','San Miguel','San José','10708','Hidalgo',475,707,'A');
+INSERT INTO DIRECCION(calle,ciudad,colonia,cp,estado,exterior,interior,municipio)VALUES('Cerrada Adipiscing. ','Nicoya','G','51616','Yucatán',486,616,'C');
+INSERT INTO DIRECCION(calle,ciudad,colonia,cp,estado,exterior,interior,municipio)VALUES('Apartado Et, Avenida','Quesada','Alajuela','21516','Estado de México',337,416,'G');
+INSERT INTO DIRECCION(calle,ciudad,colonia,cp,estado,interior,exterior,municipio)VALUES('P.O. Box 835, 1157 Sit Street','San Rafael','Alajuela','20209','Campeche',880,896,'Alajuela')
+INSERT INTO DIRECCION(calle,ciudad,colonia,cp,estado,interior,exterior,municipio)VALUES('P.O. Box 577, 9436 Ipsum Av.','San Juan (San Juan de Tibás)','SJ','12718','San Luis Potosí',221,292,'G');
+INSERT INTO DIRECCION(calle,ciudad,colonia,cp,estado,interior,exterior,municipio)VALUES('Ap #165-7853 Eu, St.','San Antonio','San José','11401','Chihuahua',578,731,'P');
+INSERT INTO DIRECCION(calle,ciudad,colonia,cp,estado,interior,exterior,municipio)VALUES('112-2162 Quisque St.','San Pablo','H','40202','Tlaxcala',322,826,'San José');
+INSERT INTO DIRECCION(calle,ciudad,colonia,cp,estado,interior,exterior,municipio)VALUES('736-4122 Nulla St.','Calle Blancos','SJ','11012','Chihuahua',142,156,'SJ');
+INSERT INTO DIRECCION(calle,ciudad,colonia,cp,estado,interior,exterior,municipio)VALUES('Ap #924-8032 Nec, Road','Calle Blancos','San José','10913','Veracruz',115,210,'San José');
+INSERT INTO DIRECCION(calle,ciudad,colonia,cp,estado,interior,exterior,municipio)VALUES('987-3299 Integer Rd.','Turrialba','Cartago','30918','Morelos',864,426,'San José');
+INSERT INTO DIRECCION(calle,ciudad,colonia,cp,estado,interior,exterior,municipio)VALUES('Ap #585-8743 Proin Av.','Mercedes','Heredia','40505','Querétaro',125,627,'C');
+INSERT INTO DIRECCION(calle,ciudad,colonia,cp,estado,interior,exterior,municipio)VALUES('Ap #265-7148 Dolor. Rd.','Nicoya','Guanacaste','50506','Guanajuato',823,982,'San José');
+INSERT INTO DIRECCION(calle,ciudad,colonia,cp,estado,interior,exterior,municipio)VALUES('P.O. Box 918, 7489 Tempus Rd.','San Rafael','Alajuela','21304','Michoacán',633,690,'A');
+
+INSERT INTO CHOFER(celular,correo,fotografia,f_registro,licencia,materno,nombre,paterno,id_direccion,bono,ganancia,contadorchofer)VALUES('12953722','iaculis.quis.pede@quisdiam.co.uk','','2018-04-04','18033893','Preston','Abra','Sheppard',1,0,0,0);
+INSERT INTO CHOFER(celular,correo,fotografia,f_registro,licencia,materno,nombre,paterno,id_direccion,bono,ganancia,contadorchofer)VALUES('20116552','est.ac.mattis@Donecnon.com','','2019-11-18','8702857','Clarke','Kadeem','Williamson',2,0,0,0);
+INSERT INTO CHOFER(celular,correo,fotografia,f_registro,licencia,materno,nombre,paterno,id_direccion,bono,ganancia,contadorchofer)VALUES('53364696','pharetra.sed@purusin.net','','2018-08-08','44520872','Solis','Zenaida','Houston',3,0,0,0);
+INSERT INTO CHOFER(celular,correo,fotografia,f_registro,licencia,materno,nombre,paterno,id_direccion,bono,ganancia,contadorchofer)VALUES('42107780','sed.orci@ametanteVivamus.org','','2019-12-13','29479513','Acosta','Philip','Hampton',4,0,0,0);
+INSERT INTO CHOFER(celular,correo,fotografia,f_registro,licencia,materno,nombre,paterno,id_direccion,bono,ganancia,contadorchofer)VALUES('88337527','at@tristique.net','','2019-12-13','40398668','Adams','Ryder','Hernandez',5,0,0,0);
+
+INSERT INTO DUENIO(celular,correo,fotografia,f_registro,licencia,materno,nombre,paterno,id_direccion,rfc)VALUES('82421574','In.mi.pede@sedduiFusce.edu','','2018-03-29','16866301','Sampson','Aimee','Whitaker',6,'1610050870599');
+INSERT INTO DUENIO(celular,correo,fotografia,f_registro,licencia,materno,nombre,paterno,id_direccion,rfc)VALUES('67393203','amet@eu.net','','2018-08-27','13708707','Sears','Jared','Shepherd',7,'1688031553999');
+INSERT INTO DUENIO(celular,correo,fotografia,f_registro,licencia,materno,nombre,paterno,id_direccion,rfc)VALUES('87966654','pellentesque@lacusQuisquepurus.co.uk','','2018-05-21','8629913','Alford','Serina','Combs',8,'1663092740999');
+INSERT INTO DUENIO(celular,correo,fotografia,f_registro,licencia,materno,nombre,paterno,id_direccion,rfc)VALUES('14229915','suscipit@eunulla.net','','2018-08-22','6832336','Owens','Julian','Ortega',9,'1671051195599');
+INSERT INTO DUENIO(celular,correo,fotografia,f_registro,licencia,materno,nombre,paterno,id_direccion,rfc)VALUES('32582204','pede.Praesent@arcuvel.ca','','2018-02-07','21853829','Pace','Chiquita','Hopper',10,'1636043023899');
+
+INSERT INTO USUARIO(casa,celular,correo,f_registro,fotografia,hora_ingreso,hora_salida,instituto,materno,nombre,paterno,id_direccion,id_tipo,contadorusuario)VALUES('48383236','45405710','malesuada@erat.net','2018-01-27','','04:07','19:33','Facultad','Atkins','Hall','Eaton',11,1,0);
+INSERT INTO USUARIO(casa,celular,correo,f_registro,fotografia,hora_ingreso,hora_salida,instituto,materno,nombre,paterno,id_direccion,id_tipo,contadorusuario)VALUES('52399482','00018759','rutrum.magna.Cras@nibh.net','2018-06-09','','03:48','08:03','Instituto','Hanson','Abbot','Morrow',12,2,0);
+INSERT INTO USUARIO(casa,celular,correo,f_registro,fotografia,hora_ingreso,hora_salida,instituto,materno,nombre,paterno,id_direccion,id_tipo,contadorusuario)VALUES('27883332','28408856','tempor@sitamet.com','2018-01-12','','03:15','17:57', 'Unidad','Garza','Drew','Morse',13,3,0);
+INSERT INTO USUARIO(casa,celular,correo,f_registro,fotografia,hora_ingreso,hora_salida,instituto,materno,nombre,paterno,id_direccion,id_tipo,contadorusuario)VALUES('15222784','55255179','Nullam@interdumNunc.net','2018-04-17','','00:38','09:22','Facultad','Elliott','Cynthia','Pate',14,1,0);
+INSERT INTO USUARIO(casa,celular,correo,f_registro,fotografia,hora_ingreso,hora_salida,instituto,materno,nombre,paterno,id_direccion,id_tipo,contadorusuario)VALUES('28396100','13637418','ante.dictum@Maurisut.co.uk','2018-12-10','','03:32','20:30','Instituto','Swanson','Samson','Sanchez',15,2,0);
+
+INSERT INTO VEHICULO(matricula,anio,capacidad,marca,modelo,pasajeros,seguro,tipo_seguro,id_chofer,id_cilindros,id_puertas,id_refaccion,id_tipocarro,id_tipogas,rfc)VALUES('1648042',1998,50,'Hyundai','malesuada.',4,'K1K 1U0','Terceros',1,1,1,1,1,1,1);
+INSERT INTO VEHICULO(matricula,anio,capacidad,marca,modelo,pasajeros,seguro,tipo_seguro,id_chofer,id_cilindros,id_puertas,id_refaccion,id_tipocarro,id_tipogas,rfc)VALUES('1669101',2003,71,'BMW','a',8,'J5D 9H4','Terceros Ampliados',2,2,2,2,2,2,2);
+INSERT INTO VEHICULO(matricula,anio,capacidad,marca,modelo,pasajeros,seguro,tipo_seguro,id_chofer,id_cilindros,id_puertas,id_refaccion,id_tipocarro,id_tipogas,rfc)VALUES('1613081',1994,52,'Nissan','eros.',6,'T2F 4U9','Terceros Ampliados',3,5,1,1,1,1,3);
+INSERT INTO VEHICULO(matricula,anio,capacidad,marca,modelo,pasajeros,seguro,tipo_seguro,id_chofer,id_cilindros,id_puertas,id_refaccion,id_tipocarro,id_tipogas,rfc)VALUES('1622011',1973,56,'Jaguar','ridiculus mus.',6,'T4O 5G8','Terceros',4,8,2,2,2,2,4);
+INSERT INTO VEHICULO(matricula,anio,capacidad,marca,modelo,pasajeros,seguro,tipo_seguro,id_chofer,id_cilindros,id_puertas,id_refaccion,id_tipocarro,id_tipogas,rfc)VALUES('1659072',2011,68,'BMW','lacinia',8,'L4E 9N9','Todo Riesgo',5,7,4,2,2,2,5);
+
+INSERT INTO MULTA(lugar,hora,agente,info,monto,matricula,licencia)VALUES('230-8570 Mollis St.','04:05','Clare Aguirre','a feugiat tellus lorem eu metus. In lorem. Donec elementum,',1833,1,1);
+INSERT INTO MULTA(lugar,hora,agente,info,monto,matricula,licencia)VALUES('3966 Nulla Ave','07:12','Candice Boyer','lorem, sit amet ultricies sem magna nec quam. Curabitur vel',1988,2,2);
+INSERT INTO MULTA(lugar,hora,agente,info,monto,matricula,licencia)VALUES('9721 Duis Av.','20:40','Riley Page','massa. Integer vitae nibh. Donec est mauris, rhoncus id, mollis',4519,3,3);
+
+
 
